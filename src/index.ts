19eb4e698d24292d7fb359e7fc09a43269f58fbe
Natallia <natashapridanova17@gmail.com>
2022-08-18 18:08:49 +0300

    feat: add functions when dom is loaded

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import './style.scss';
 import axios from 'axios';
 import * as Materialize from '@materializecss/materialize';
+import { modalsOn } from './components/contentLoaded/materialize';
+import { activateForms, activateHeaders } from './components/contentLoaded/dom';
+
+document.addEventListener('DOMContentLoaded', () =>{
+  modalsOn();
+  activateHeaders();
+  activateForms();
+});
