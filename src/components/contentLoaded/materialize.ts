76c127a343c6b44faf9d29502a4ae4f2fd4f721b
Natallia <natashapridanova17@gmail.com>
2022-08-18 18:03:12 +0300

    feat: create authorization form

---
+++ src/components/contentLoaded/materialize.ts
@@ -0,0 +1,12 @@
+import * as Materialize from '@materializecss/materialize';
+
+const modals = document.querySelectorAll('.modal') as NodeListOf<HTMLElement>;
+const authModal = document.querySelector('.auth-modal') as HTMLElement;
+export function modalsOn(): void {
+  Materialize.Modal.init(modals, {});
+}
+export function authModalOff(): void {
+  const modal = Materialize.Modal.getInstance(authModal);
+modal.close();
+}
+
