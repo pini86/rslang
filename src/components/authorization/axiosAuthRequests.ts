afe8418244ec201da8f9cbc1c3aba0902d04191b
Natallia <natashapridanova17@gmail.com>
2022-08-22 15:22:36 +0300

    fix: fix axios interceptors

--- src/components/authorization/axiosAuthRequests.ts
+++ src/components/authorization/axiosAuthRequests.ts
@@ -1,7 +1,5 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import jwt from 'jsonwebtoken';
-import api from '../../api/api';
-import saveToken from './saveToStorage';
 
 const axiosAuth: AxiosInstance = axios.create();
 axiosAuth.interceptors.request.use((requestConfig: AxiosRequestConfig): AxiosRequestConfig => {
@@ -22,19 +20,5 @@ axiosAuth.interceptors.request.use((requestConfig: AxiosRequestConfig): AxiosReq
   }
   return requestConfig;
 });
-
-axios.interceptors.response.use(
-  (response: AxiosResponse): AxiosResponse => response,
-  (error) => {
-    if (
-      error.statusText === 'Access token is missing or invalid' ||
-      error.statusText === 'Access token is missing, expired or invalid'
-    ) {
-      api.getNewIUserTokens().then((tokenData) => {
-        saveToken(JSON.stringify(tokenData));
-      });
-    }
-  }
-);
 
 export default axiosAuth;
