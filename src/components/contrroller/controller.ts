dcccaaef5d598003083c1b25ee32fd284c54db8f
Mikhail Druzhinin <pini86@mail.ru>
2022-08-22 18:22:00 +0300

    feat: add restore from sessionstorage

--- src/components/contrroller/controller.ts
+++ src/components/contrroller/controller.ts
@@ -6,12 +6,12 @@ import Statistics from '../../pages/statistics/statistics';
 import Authorization from '../../pages/authorization/authorization';
 
 enum EPages {
-  auth = 'auth',
-  main = 'main',
-  electronBook = 'electronBook',
-  audiocall = 'audiocall',
-  sprint = 'sprint',
-  statistics = 'statistics',
+  auth = 'Auth',
+  main = 'Main',
+  ebook = 'Ebook',
+  audiocall = 'Audiocall',
+  sprint = 'Sprint',
+  statistics = 'Statistics',
 }
 
 export default class Controller {
@@ -21,26 +21,18 @@ export default class Controller {
 
   mainView = new Main();
 
-  /* constructor() {
-   
-  } */
-
   initApp(): void {
-    this.addHeaderListeners(); // сюда добавить проверку в sessionstorage на предмет сохраненной страницы
+    this.createView();
   }
 
-  addHeaderListeners(): void {
+  createView(): void {
     const mainBtn = document.getElementById('main') as HTMLElement;
-    const electronBookBtn = document.getElementById('ebook') as HTMLElement;
-    const audioCallBtn = document.getElementById('audiocall') as HTMLElement;
+    const ebookBtn = document.getElementById('ebook') as HTMLElement;
+    const audiocallBtn = document.getElementById('audiocall') as HTMLElement;
     const sprintBtn = document.getElementById('sprint') as HTMLElement;
-    const autBtn = document.getElementById('authorization') as HTMLElement;
+    const authBtn = document.getElementById('authorization') as HTMLElement;
     const statisticsBtn = document.getElementById('statistics') as HTMLElement;
     const iconMenu = document.getElementById('icon-menu') as HTMLElement;
-
-    // console.log( iconMenu);
-
-    Controller.setActiveMenuItem(mainBtn);
 
     iconMenu.addEventListener('click', () => {
       if (iconMenu.classList.contains('icon-menu--active')) {
@@ -50,60 +42,92 @@ export default class Controller {
       }
     });
 
-    autBtn.addEventListener('click', (): void => {
+    authBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.auth) return;
       this.currentPage = EPages.auth;
-      Controller.setActiveMenuItem(autBtn);
+      this.setActiveMenuItem(authBtn);
       this.mainView = new Authorization();
     });
 
     mainBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.main) return;
       this.currentPage = EPages.main;
       this.mainView = new Main();
       Controller.toggleHeaderMenu('close');
-      Controller.setActiveMenuItem(mainBtn);
+      this.setActiveMenuItem(mainBtn);
     });
 
-    electronBookBtn.addEventListener('click', (): void => {
-      this.currentPage = EPages.electronBook;
+    ebookBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.ebook) return;
+      this.currentPage = EPages.ebook;
       this.mainView = new Ebook();
       Controller.toggleHeaderMenu('close');
-      Controller.setActiveMenuItem(electronBookBtn);
+      this.setActiveMenuItem(ebookBtn);
     });
 
-    audioCallBtn.addEventListener('click', (): void => {
+    audiocallBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.audiocall) return;
       this.currentPage = EPages.audiocall;
       this.mainView = new Audiocall();
       Controller.toggleHeaderMenu('close');
-      Controller.setActiveMenuItem(audioCallBtn);
+      this.setActiveMenuItem(audiocallBtn);
     });
 
     sprintBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.sprint) return;
       this.currentPage = EPages.sprint;
       this.mainView = new Sprint();
       Controller.toggleHeaderMenu('close');
-      Controller.setActiveMenuItem(sprintBtn);
+      this.setActiveMenuItem(sprintBtn);
     });
 
     statisticsBtn.addEventListener('click', (): void => {
+      if (this.currentPage === EPages.statistics) return;
       this.currentPage = EPages.statistics;
       this.mainView = new Statistics();
       Controller.toggleHeaderMenu('close');
-      Controller.setActiveMenuItem(statisticsBtn);
+      this.setActiveMenuItem(statisticsBtn);
     });
+
+    this.checkSessionStorage();
+    
+    switch (this.currentPage) {
+      case 'Auth':
+        this.setActiveMenuItem(authBtn);
+        this.mainView = new Authorization();
+        break;
+      case 'Ebook':
+        this.setActiveMenuItem(ebookBtn);
+        this.mainView = new Ebook();
+        break;
+      case 'Audiocall':
+        this.setActiveMenuItem(audiocallBtn);
+        this.mainView = new Audiocall();
+        break;
+      case 'Sprint':
+        this.setActiveMenuItem(sprintBtn);
+        this.mainView = new Sprint();
+        break;
+      case 'Statistics':
+        this.setActiveMenuItem(statisticsBtn);
+        this.mainView = new Statistics();
+        break;
+      default:
+        this.setActiveMenuItem(mainBtn);
+    }
   }
 
-  static setActiveMenuItem(menuItem: HTMLElement): void {
+  setActiveMenuItem(menuItem: HTMLElement): void {
     const bodyMenu = document.querySelector('.body-menu') as HTMLElement;
     const menuItems = [...bodyMenu.children] as HTMLElement[];
     menuItems.forEach((li) => li.classList.remove('active'));
     menuItem.classList.add('active');
+    this.setSessionStorage();
   }
 
   static toggleHeaderMenu(action: string): void {
-    
     const iconMenu = document.getElementById('icon-menu') as HTMLElement;
     const bodyMenu = document.getElementById('body-menu') as HTMLElement;
-
     if (action === 'close') {
       iconMenu.classList.remove('icon-menu--active');
       bodyMenu.classList.remove('body-menu--active');
@@ -118,5 +142,9 @@ export default class Controller {
     if (getKey) {
       this.currentPage = getKey;
     }
+  }
+
+  setSessionStorage(): void {
+    sessionStorage.setItem(this.keyStorage, this.currentPage);
   }
 }
