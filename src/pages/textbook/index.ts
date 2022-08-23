3d05269ca82636e6117d2cd79499fc66b9f5b845
User0k <glucus2@yandex.ru>
2022-08-23 23:06:49 +0300

    feat: implement dynamic pagination

--- src/pages/textbook/index.ts
+++ src/pages/textbook/index.ts
@@ -1,8 +1,8 @@
 import './style.scss';
 import createDiffPanel from './difficulty-panel';
 import renderCards from './render-cards';
+import { initPagination } from './pagination';
 
-const main = document.querySelector('#textbook') as HTMLElement;
-
-createDiffPanel(main);
+createDiffPanel();
 renderCards();
+initPagination();
