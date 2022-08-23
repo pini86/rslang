2e0b5eb1b021d25727ba2b3a3f8b91f7d8f141bd
User0k <glucus2@yandex.ru>
2022-08-23 11:57:32 +0300

    feat: add cardLevels file to generate levels and colors

---
+++ src/pages/textbook/card-Levels.ts
@@ -0,0 +1,11 @@
+const cardLevels = [
+  { color: 'green lighten-1', diffClassName: 'a1' },
+  { color: 'light-green darken-3', diffClassName: 'a2' },
+  { color: 'teal lighten-1', diffClassName: 'b1' },
+  { color: 'teal darken-2', diffClassName: 'b2' },
+  { color: 'orange', diffClassName: 'c1' },
+  { color: 'deep-orange', diffClassName: 'c2' },
+  { color: 'red', diffClassName: 'u' },
+];
+
+export default cardLevels;
