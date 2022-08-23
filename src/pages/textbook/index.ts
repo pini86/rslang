df65a763185890209dbf3c941fe9c96f17fdbf7f
User0k <glucus2@yandex.ru>
2022-08-23 12:00:24 +0300

    feat: create card render, move difficulty panel and cards from html, add paddings to pagination

--- src/pages/textbook/index.ts
+++ src/pages/textbook/index.ts
@@ -1,1 +1,8 @@
 import './style.scss';
+import createDiffPanel from './difficulty-panel';
+import renderCards from './render-cards';
+
+const main = document.querySelector('#textbook') as HTMLElement;
+
+createDiffPanel(main);
+renderCards();
---
+++ src/pages/textbook/render-cards.ts
@@ -0,0 +1,89 @@
+import cardLevels from './card-Levels';
+import api from '../../api/api';
+
+const BASE = 'https://rs-lang-rsschool-task.herokuapp.com';
+const container = document.querySelector('main .container') as HTMLElement;
+
+function generateCard(
+  group: number,
+  id: string,
+  image: string,
+  word: string,
+  transcription: string,
+  translation: string,
+  textExample: string,
+  textTranslate: string,
+  meaning: string,
+  meaningTranslate: string
+) {
+  return `
+    <div class="row d-flex">
+      <div class="col d-flex m10 s12 white card-wrapper">
+        <div class="col image-wrapper">
+          <div class="card">
+            <div class="card-image z-depth-3">
+              <img src=${BASE}/${image}>
+              <div class="card-title">
+                <div>
+                  <span class="word">${word}</span>
+                  <span class="transcription">${transcription}</span>
+                </div>
+                <span class="translation">(${translation})</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card-content d-flex">
+          <div class="content-example">
+            <p class="content-phrase">${textExample}</p>
+            <p class="content-translation grey-text darken-3">${textTranslate}</p>
+          </div>
+          <div class="content-example">
+            <p class="content-phrase">${meaning}</p>
+            <p class="content-translation grey-text darken-3">${meaningTranslate}</p>
+          </div>
+          <div class="card-action">
+            <button id="${id}" class="btn ${cardLevels[group].color} btn-listen">
+              <i class="material-icons">volume_up</i>
+            </button>
+            <button id="${id}" class="btn ${cardLevels[group].color} btn-hard">В трудные</button>
+            <button id="${id}" class="btn ${cardLevels[group].color} btn-remove-hard disabled">Из трудных</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+export default async function renderCards(group = 0, page = 0) {
+  const words = await api.getWords(group, page);
+  let cardsToRender = '';
+  words.forEach((w) => {
+    const {
+      id,
+      image,
+      word,
+      transcription,
+      wordTranslate,
+      textExample,
+      textExampleTranslate,
+      textMeaning,
+      textMeaningTranslate,
+    } = w;
+
+    cardsToRender += generateCard(
+      group,
+      id,
+      image,
+      word,
+      transcription,
+      wordTranslate,
+      textExample,
+      textExampleTranslate,
+      textMeaning,
+      textMeaningTranslate
+    );
+  });
+
+  container.innerHTML = cardsToRender;
+}
