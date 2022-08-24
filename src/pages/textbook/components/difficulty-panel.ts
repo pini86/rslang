57075519ed5ccd630591bfacb03c830ac968d53a
User0k <glucus2@yandex.ru>
2022-08-24 19:32:10 +0300

    feat: add listener to panel, bind with state, move into components folder

---
+++ src/pages/textbook/components/difficulty-panel.ts
@@ -0,0 +1,44 @@
+import cardLevels from '../card-Levels';
+import state from '../state';
+import renderCards from './cards';
+import initPagination from './pagination';
+
+const main = document.querySelector('#textbook') as HTMLElement;
+let { curGroup, curPage } = state;
+
+export default function createDiffPanel() {
+  const textBookLevels = document.createElement('ul');
+  textBookLevels.classList.add('collection', 'textbook-levels');
+  let levelsToRender = '';
+
+  cardLevels.forEach(({ color, difficultyId }, i) => {
+    const isActive = i === curGroup ? 'level-active' : '';
+    levelsToRender += `
+      <li class="collection-item">
+        <span id="${difficultyId}" class="btn ${color} ${isActive}">${difficultyId}</span>
+      </li>
+    `;
+  });
+
+  textBookLevels.innerHTML = levelsToRender;
+
+  textBookLevels.addEventListener('click', (e) => {
+    const el = e.target as HTMLElement;
+    let curId = cardLevels[curGroup].difficultyId;
+
+    if (el.classList.contains('btn') && el.getAttribute('id') !== curId) {
+      textBookLevels.querySelector(`#${curId}`)?.classList.remove('level-active');
+      el.classList.add('level-active');
+      curId = el.getAttribute('id') as string;
+      curGroup = cardLevels.findIndex((level) => level.difficultyId === curId);
+      curPage = 0;
+      state.curGroup = curGroup;
+      sessionStorage.setItem('page', `${curPage}`);
+      sessionStorage.setItem('group', `${curGroup}`);
+      renderCards(curGroup, curPage);
+      initPagination();
+    }
+  });
+
+  main.prepend(textBookLevels);
+}
