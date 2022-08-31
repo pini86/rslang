f2ca2a3428e118d320e5b9cbafd82b987c13ecab
User0k <glucus2@yandex.ru>
2022-09-01 02:18:43 +0300

    feat: add game panel

---
+++ src/components/ebook/game-panel.ts
@@ -0,0 +1,22 @@
+import state from '../../pages/ebook/state';
+
+const main = document.querySelector('main') as HTMLElement;
+
+export default function createGamePanel() {
+  const gamePanel = document.createElement('ul');
+  gamePanel.classList.add('collection', 'game-panel');
+  const isActive = state.easyCount === 20 ? 'disabled' : '';
+  gamePanel.innerHTML = `
+    <li class="collection-item avatar btn ${isActive}">
+      <a href="#">
+        <img src="../../assets/icons/audiocall.png" alt="audiocall" class="circle">
+      </a>
+    </li>
+    <li class="collection-item avatar btn ${isActive}">
+      <a href="#">
+        <img src="../../assets/icons/sprint.svg" alt="audiocall" class="circle">
+      </a>
+    </li>
+  `
+  main.prepend(gamePanel);
+}
--- src/pages/ebook/ebook.ts
+++ src/pages/ebook/ebook.ts
@@ -1,5 +1,6 @@
 import state from './state';
 import createDiffPanel from '../../components/ebook/difficulty-panel';
+import createGamePanel from '../../components/ebook/game-panel';
 import renderCards from '../../components/ebook/cards';
 import { initPagination } from '../../components/ebook/pagination';
 import renderDifficultCards from '../../components/ebook/difficult-cards';
@@ -15,6 +16,7 @@ export class Ebook {
 
 export function initEbook() {
   createDiffPanel();
+  createGamePanel();
   if (state.curGroup !== 6) {
     renderCards();
     initPagination();
