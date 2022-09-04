038e47a9449fc950831ce80f7a438495608d7e54
User0k <glucus2@yandex.ru>
2022-09-04 10:43:47 +0300

    fix: edit game-panel buttons

--- src/components/ebook/game-panel.ts
+++ src/components/ebook/game-panel.ts
@@ -1,4 +1,4 @@
-import { TOTAL_PAGES_AMOUNT } from '../../constants/constants';
+import { WORDS_PER_PAGE } from '../../constants/constants';
 import state from '../../pages/ebook/state';
 
 const main = document.querySelector('main') as HTMLElement;
@@ -6,18 +6,14 @@ const main = document.querySelector('main') as HTMLElement;
 export default function createGamePanel() {
   const gamePanel = document.createElement('ul');
   gamePanel.classList.add('collection', 'game-panel');
-  const isActive = state.easyCount === TOTAL_PAGES_AMOUNT - 1 ? 'disabled' : '';
+  const isActive = state.easyCount === WORDS_PER_PAGE ? 'disabled' : '';
   gamePanel.innerHTML = `
-    <li class="collection-item avatar btn ${isActive}">
-      <a href="#">
-        <img src="../../assets/icons/audiocall.png" alt="audiocall" class="circle">
-      </a>
-    </li>
-    <li class="collection-item avatar btn ${isActive}">
-      <a href="#">
-        <img src="../../assets/icons/sprint.svg" alt="audiocall" class="circle">
-      </a>
-    </li>
+    <button class="btn cyan darken-1 btn-audiocall ${isActive}">
+      <i class="material-icons">music_note</i>
+    </button>
+    <button class="btn cyan darken-1 btn-sprint ${isActive}">
+      <i class="material-icons">directions_run</i>
+    </button>
   `
   main.prepend(gamePanel);
 }
