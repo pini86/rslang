4a8789d0fbbc4ac5ae9b783b00b1e694f31aca1d
User0k <glucus2@yandex.ru>
2022-09-05 07:15:05 +0300

    feat: add new styles to audiocall-game

--- src/components/audiocall/startMode.ts
+++ src/components/audiocall/startMode.ts
@@ -5,15 +5,27 @@ export default class StartMode {
     this.gameContent = document.querySelector('main div.container') as HTMLElement;
     this.gameContent.innerHTML = `<div class="audiocall"><div class="audiocall__wrapper">
     <h5 class="audiocall__start-header">Выбери уровень, чтобы начать игру</h5>
-  
-    <div class="audiocall__levels">
-      <button class="audiocall__level waves-light waves-effect yellow active">A1</button>
-      <button class="audiocall__level waves-light waves-effect red darken-1">A2</button>
-      <button class="audiocall__level waves-light waves-effect pink lighten-3">B1</button>
-      <button class="audiocall__level waves-light waves-effect cyan darken">B2</button>
-      <button class="audiocall__level waves-light waves-effect light-green accent-1">C1</button>
-      <button class="audiocall__level waves-light waves-effect indigo darken-3">C2</button>
-    </div>
+
+    <ul class="collection audiocall__levels">
+      <li class="collection-item">
+        <button class="audiocall__level btn green lighten-1 active">A1</button>
+      </li>
+      <li class="collection-item">
+        <button class="audiocall__level btn light-green darken-3">A2</button>
+      </li>
+      <li class="collection-item">
+        <button class="audiocall__level btn cyan darken-1">B1</button>
+      </li>
+      <li class="collection-item">
+        <button class="audiocall__level btn teal darken-2">B2</button>
+      </li>
+      <li class="collection-item">
+        <button class="audiocall__level btn orange">C1</button>
+      </li>
+      <li class="collection-item">
+      <button class="audiocall__level btn pink lighten-3">C2</button>
+      </li>
+    </ul>
     <button class="audiocall__start-btn waves-effect waves-light btn">начать</button>
     <p class="audiocall__start-header">Используй мышку или клавиши 1-5 и пробел</p>
   </div>
